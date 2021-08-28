6a8da664e955a587f5149e15cc7317b12c74d9ec
MohaiminulEraj <eraj471@gmaIl.com>
2021-08-28 12:25:40 +0600

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ module.exports = {
     // NEXTAUTH_URL: 'http://localhost:3000',
     DB_LOCAL_URI: 'mongodb://localhost:27017/careforyou',
     CLOUDINARY_CLOUD_NAME: 'careforyou',
-    CLOUDINARY_API_KEY: '381723694724926',
-    CLOUDINARY_API_SECRET: 'kYIcElHuwFT8TCW2dPV9V5FaGd4',
+    CLOUDINARY_API_KEY: '',
+    CLOUDINARY_API_SECRET: '',
 
-    SMTP_HOST: 'smtp.mailtrap.io',
-    SMTP_PORT: '2525',
-    SMTP_USER: '33b8cba2e225ee',
-    SMTP_PASSWORD: 'c1c491564a2f21',
+    SMTP_HOST: '',
+    SMTP_PORT: '',
+    SMTP_USER: '',
+    SMTP_PASSWORD: '',
 
     STMP_FROM_NAME: 'CareForYou',
     STMP_FROM_EMAIL: 'noreply@careforyou.com'
